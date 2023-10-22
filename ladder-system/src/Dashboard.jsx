18c86e915245d80d5e72f00c0d669cd0f9d4adbf
Joshua Shor <44251534+JoshShor@users.noreply.github.com>
2023-10-22 10:48:47 -0700

    fixed errors on dashboard page

--- ladder-system/src/Dashboard.jsx
+++ ladder-system/src/Dashboard.jsx
@@ -6,34 +6,34 @@ export function Dashboard() {
         <div>
                 <h3>dashboard</h3>
                 <Link to="/Ladder">
-                    <button class="dash_btn">NGVB Volleyball Ladder Spring 2023</button>
+                    <button className="dash_btn">NGVB Volleyball Ladder Spring 2023</button>
                 </Link>
                 <br></br>
                 <Link to="/Ladder">
-                    <button class="dash_btn">NGVB Volleyball 2's Ladder Spring 2023</button>
+                    <button className="dash_btn">NGVB Volleyball 2's Ladder Spring 2023</button>
                 </Link>
                 <br></br>
                 <Link to="/Ladder">
-                    <button class="dash_btn">6v6 casual</button><br></br>
+                    <button className="dash_btn">6v6 casual</button><br></br>
                 </Link>
                 <br></br>
 
                 <Link to="/Ladder">
-                    <button class="dash_btn">Ladder</button>
+                    <button className="dash_btn">Ladder</button>
                 </Link>
                 <br></br>
                 <Link to="/Team">
-                    <button class="dash_btn">Team</button>
+                    <button className="dash_btn">Team</button>
                 </Link>
                 <br></br>
                 <br></br>
                 <Link to="/">
-                    <a href="\#" class="dash_lnk">Rules</a>
+                    <button className="dash_lnk">Rules</button>
                 </Link>
                 <br></br>
                 <Link to="/">
-                <a href="\#" class="dash_lnk">Back</a>
+                    <button className="dash_lnk">Back</button>
                 </Link>
-                </div>
+        </div>
     )
 }
