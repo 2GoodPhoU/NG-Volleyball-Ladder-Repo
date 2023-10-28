caa598b87550143bedbc2a88bb267d3396657abf
Adrian <herna552@csusm.edu>
2023-10-28 16:40:43 -0700

    update

--- ladder-system/src/Dashboard.jsx
+++ ladder-system/src/Dashboard.jsx
@@ -1,39 +1,51 @@
-import { Link } from "react-router-dom";
 import './App.css';
 
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+
 export function Dashboard() {
+    const [ladderTournaments, setLadderTournaments] = useState([]);
+
+    useEffect(() => {
+        getLadder();
+    }, []);
+
+    async function getLadder() {
+        const { data } = await supabase
+        .from('ladder_tournaments')
+        .select();
+        
+        setLadderTournaments(data);
+    }
+
     return (
         <div>
-                <h3>dashboard</h3>
-                <Link to="/Ladder">
-                    <button class="dash_btn">NGVB Volleyball Ladder Spring 2023</button>
-                </Link>
-                <br></br>
-                <Link to="/Ladder">
-                    <button class="dash_btn">NGVB Volleyball 2's Ladder Spring 2023</button>
-                </Link>
-                <br></br>
-                <Link to="/Ladder">
-                    <button class="dash_btn">6v6 casual</button><br></br>
-                </Link>
-                <br></br>
-
-                <Link to="/Ladder">
-                    <button class="dash_btn">Ladder</button>
-                </Link>
-                <br></br>
-                <Link to="/Team">
-                    <button class="dash_btn">Team</button>
-                </Link>
-                <br></br>
-                <br></br>
-                <Link to="/">
-                    <a href="\#" class="dash_lnk">Rules</a>
-                </Link>
-                <br></br>
-                <Link to="/">
-                <a href="\#" class="dash_lnk">Back</a>
-                </Link>
-                </div>
+            <h3>Dashboard</h3>
+            <div>
+                {ladderTournaments.map((tournament, i) => 
+                    <div key={i}>
+                        <Link to="/Ladder">
+                            <button className="dash_btn">
+                                { tournament.ladder_tournament_name }
+                            </button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+            
+            <Link to="/Team">
+                <button className="dash_btn">Team</button>
+            </Link>
+            <br></br>
+            <br></br>
+            <Link to="/">
+                <button className="dash_lnk">Rules</button>
+            </Link>
+            <br></br>
+            <Link to="/">
+                <button className="dash_lnk">Back</button>
+            </Link>
+        </div>
     )
 }
