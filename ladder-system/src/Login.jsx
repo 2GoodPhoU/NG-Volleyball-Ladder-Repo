32361a5f2abd123f58878629c634246f66db80ff
EricPaulin <ericpaulin78055@gmail.com>
2023-10-30 12:16:38 -0700

    personal change log added

--- ladder-system/src/Login.jsx
+++ ladder-system/src/Login.jsx
@@ -6,6 +6,18 @@ import MailIcon from '@mui/icons-material/Mail';
 
 import './App.css';
 
+/* Login */
+// MUI overhaul (login) (v1 or v2)
+// Remove Information div
+
+/* Register */
+// MUI overhaul (register)
+// add fName, lName
+// edit terms and conditions (boolean)
+
+/* Style */
+// add default font (*)
+// move Popup to components folder
 
 /*
 CREATE TABLE users (
@@ -101,7 +113,6 @@ export const Login = (props) => {
 
                 <Link to="/Register" className="link"> Create Account </Link>
                 <Link to="/Dashboard" className="link"> Continue as Guest </Link>
-                <div className="link"> Information </div>
 
                 <button className="link">
                  <MailIcon/>
