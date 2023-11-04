0e86334eb7ec75fd25c344c962842f1f1a0351bd
EricPaulin <ericpaulin78055@gmail.com>
2023-11-03 23:30:23 -0700

    Manage Users UI
    - added Table Pagination
    - fixed sizing

--- ladder-system/src/pages/UserManager.jsx
+++ ladder-system/src/pages/UserManager.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,19 +7,26 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from "@mui/material/TablePagination";
 
 import { Paper, styled, Typography, Container, Box, Button } from '@mui/material';
+
+
 
 import IconButton from '@mui/material/IconButton';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
-import Popup from "../components/Popup";
-
-
-// page only accessible by Ladder Admin and Admin
+/*
+User Manager (v.1)
+  - delete user
+  - edit user
+  - Ladder Moderator and Admin only
+User Manager (v.2)
+  - (Admin) switch between tournaments
+  - (Ladder Moderator) restricted to their tournament
+*/
 
 /* Cell and Row Styling */
-
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
@@ -41,11 +47,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-/* Dropdown / Popup */
-
-
-
-
 /* Testing Data */
 
 // return data
@@ -58,66 +59,91 @@ const users = [
   createData('User1', 'Team1', 'Member'),
   createData('User2', 'Team2', 'Member'),
   createData('User3', 'Team3', 'Team Leader'),
-  createData('User4', 'Team4', 'Ladder Admin'),
+  createData('User4', 'Team4', 'Ladder Moderator'),
   createData('User5', 'Team5', 'Admin'),
+  createData('User6', 'Team6', 'Team Leader'),
 ];
 
 
 /* User Manager Page */
 export function UserManager() {
 
-  // Popup State
-  const [isPopupOpen, togglePopup] = useState(false);
+  // Table Pagination
+  const [pg, setpg] = React.useState(0); 
+  const [rpg, setrpg] = React.useState(5); 
+
+  function handleChangePage(event, newpage) {
+    setpg(newpage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setrpg(parseInt(event.target.value, 10));
+    setpg(0);
+  }
+
 
   return (
-    <Container component="main" maxWidth="">
+    <Container component="main" maxWidth="md">
       <Box
         sx={{
           marginTop: 8,
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
+          alignItems: 'center'
         }}
       >
 
         {/* Title */}
 
-        <Typography component="h1" variant="h5" sx={{marginBottom: 2}}>
+        <Typography component="h1" variant="h5" sx={{ marginBottom: 2 }}>
           User Manager
         </Typography>
 
         {/* Table */}
         <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 100 }} aria-label="customized table">
+          <Table>
+            {/* Table Header */}
             <TableHead>
               <TableRow>
-                <StyledTableCell align="center" sx={{ width: '25%' }}> Username </StyledTableCell>
-                <StyledTableCell align="right" sx={{ width: '25%' }}> Team </StyledTableCell>
-                <StyledTableCell align="right" sx={{ width: '45%' }}>Role</StyledTableCell>
-                <StyledTableCell align="center" sx={{ width: '5%' }}></StyledTableCell>
+                <StyledTableCell align="center"> Username </StyledTableCell>
+                <StyledTableCell align="center"> Team </StyledTableCell>
+                <StyledTableCell align="center">Role</StyledTableCell>
+                <StyledTableCell align="center"></StyledTableCell>
               </TableRow>
             </TableHead>
+            {/* Table Body */}
             <TableBody>
-              {users.map((user) => (
-                <StyledTableRow key={user.name}>
-                  <StyledTableCell align="center" sx={{ width: '25%' }} scope="row">{user.name}</StyledTableCell>
-                  <StyledTableCell align="right" sx={{ width: '25%' }}>{user.team}</StyledTableCell>
-                  <StyledTableCell align="right" sx={{ width: '45%' }}>{user.role}</StyledTableCell>
-                  <StyledTableCell align="right" sx={{ width: '5%' }}>  <IconButton disableRipple> <ManageAccountsIcon onClick={() => togglePopup(true)} /> </IconButton> </StyledTableCell>
-                  {isPopupOpen ? <Popup
-                                className="userPopup"
-                                title="[User]'s Account Information"
-                                text="*insert terms here*"
-                                closePopup={() => togglePopup(false)} /> : null}
+            {users.slice(pg * rpg, pg * rpg + rpg).map((user) => (
+                <StyledTableRow key={user.name} sx={{ "&:last-child td,  &:last-child th": { border: 0 } }}>
+                  <StyledTableCell align="center" sx={{ width: '30%' }} scope="user">{user.name}</StyledTableCell>
+                  <StyledTableCell align="center" sx={{ width: '30%' }}>{user.team}</StyledTableCell>
+                  <StyledTableCell align="center" sx={{ width: '30%' }}>{user.role}</StyledTableCell>
+                  <StyledTableCell align="right" sx={{ width: '10%' }}>
+                    {/* Edit User */}
+                    <IconButton>
+                      <ManageAccountsIcon />
+                    </IconButton>
+
+                  </StyledTableCell>
                 </StyledTableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-
+         {/* Table Footer */}
+         <TablePagination 
+                rowsPerPageOptions={[5, 10, 25]} 
+                component="div"
+                count={users.length} 
+                rowsPerPage={rpg} 
+                page={pg}
+                onPageChange={handleChangePage} 
+                onRowsPerPageChange={handleChangeRowsPerPage} 
+          />
+          
         {/* Return */}
         <Link to="/Settings">
-          <Button variant="text" sx={{marginTop: 2}}>Back</Button>
+          <Button variant="text" sx={{ marginTop: 2 }}>Back</Button>
         </Link>
 
       </Box>
